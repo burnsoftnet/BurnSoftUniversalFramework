48439b34cdace3030e35492136d17825097fe6f2
Joe M <joe.mireles@burnsoft.net>
2018-11-12 21:04:07 -0500

    #1 Added new test for BurnSoftGeneral

---
+++ BurnSoftUniversalTests/BurnSoftGeneralTest.m
@@ -0,0 +1,37 @@
+//
+//  BurnSoftGeneralTest.m
+//  BurnSoftUniversalTests
+//
+//  Created by burnsoft on 11/12/18.
+//  Copyright © 2018 burnsoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BurnSoftGeneralTest : XCTestCase
+
+@end
+
+@implementation BurnSoftGeneralTest
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
